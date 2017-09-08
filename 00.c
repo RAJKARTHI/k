a5446d6a7fa3f5ad1b7aa7e5ca2ae13402df6267
RAJKARTHI <31534375+RAJKARTHI@users.noreply.github.com>
2017-09-08 16:30:27 +0530

    Create 00.c

---
+++ 00.c
@@ -0,0 +1,72 @@
+<html>
+<title>Resume</title>
+<head>
+<style type="text/css">
+h3{
+font-family:"Arial";
+font-size:18px;
+text-decoration-style:underline;
+}
+blockquote {
+  background: #f9f9f9;
+  border-left: 10px solid #ccc;
+  margin: 1.5em 10px;
+  padding: 0.5em 10px;
+}
+</style>
+<h1 align=CENTER>RESUME</h1>
+</head>
+<body>
+<h3>BASIC INFO</h3>
+<p>Name       :-<b>karthi</b><br/>
+Degree        :-<b>B.TECH IT</b><br/>
+Contact Number:- <b>9876543210</b><br/>
+E-Mail ID     :- <b>rajeshvanith@gmail.com@gmail.com</b><br/>
+Present Address:-<b>100A/1,Bye pass road,karur.</b></p>
+<h3>BASIC ACADEMIC CREDENTIALS</h3>
+<table>
+<caption>Education Details</caption>
+<tr>
+<th><b>Qualification</b></th>
+<th><b>Board/University</b></th>
+<th><b>Percentage</b></th>
+<th><b>Year passed out</b></th>
+</tr>
+<tr>
+<td>10<sup>th</sup></td>
+<td>State Board</td>
+<td>78.9%</td>
+<td>2013</td>
+</tr>
+<tr>
+<td>12<sup>th</sup></td>
+<td>State Board</td>
+<td>88.3%</td>
+<td>2015</td>
+</tr>
+<tr>
+<td>B.E-CSE</td>
+<td>Kumaraguru College of Technology</td>
+<td>60%</td>
+<td>2019(MAY)</td>
+</tr>
+</table>
+<h3>MAIN PROJECT</h3>
+<table border=1>
+<tr>
+<td>Title</td>
+<td>EasyConnect</td>
+</tr>
+<tr>
+<tr>
+<td>Technology Used</td>
+<td>C# and ADO.net</td>
+</tr>
+<tr>
+</table>
+<h3>IT PROFICIENCY</h3>
+<p><li>C<li>C++<li>Java</p>
+<h3>DECLARATION</h3>
+<p>The above Declared Information are true.</p>
+</body>
+</html>
